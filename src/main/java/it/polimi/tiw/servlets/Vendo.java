2c630275298517dce400c28458ab96e611f88821
ClaudioDebla03 <claudio.deblasio@mail.polimi.it>
2025-07-10 17:44:21 +0200

    seconda lista vendo

--- src/main/java/it/polimi/tiw/servlets/Vendo.java
+++ src/main/java/it/polimi/tiw/servlets/Vendo.java
@@ -179,7 +179,7 @@ public class Vendo extends HttpServlet {
 
             ctx.setVariable("asteAperte", asteAperte);
             ctx.setVariable("asteChiuse", asteChiuse);
-            ctx.setVariable("arteChiuseconArticoli", asteChiuseconArticoli);
+            ctx.setVariable("asteChiuseconArticoli", asteChiuseconArticoli);
             ctx.setVariable("asteConArticoli", asteConArticoli);
             ctx.setVariable("tempoMancanteMap", tempoMancanteMap);
             ctx.setVariable("dateFormattateMap", dateFormattateMap);
